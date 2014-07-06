3982a73a6d06195f0bee9aeb9868b7779fe536d5
kgao1997 <kgao1997@gmail.com>
2014-07-06 09:23:42 -0400

    Corrected Commit

---
+++ logic.php
@@ -0,0 +1,52 @@
+<html>
+<body>
+Your password is <?php echo $_POST["number"]; ?>
+
+
+
+<?php 
+
+// Words Array starts here
+$wordsarray[0] = "Armada";
+$wordsarray[1] = "Blaster";
+$wordsarray[2] = "Carry";
+$wordsarray[3] = "Dingerdonger";
+$wordsarray[4] = "Evilgeniuses";
+$wordsarray[5] = "Fishtail";
+$wordsarray[6] = "GreenLine";
+$wordsarray[7] = "Highlander";
+$wordsarray[8] = "Icarus";
+$wordsarray[9] = "Jack";
+$wordsarray[10] = "Killswitch";
+$wordsarray[11] = "LMQ";
+$wordsarray[12] = "Metal";
+$wordsarray[13] = "Nashor";
+$wordsarray[14] = "Optimus";
+$wordsarray[15] = "Pancake";
+$wordsarray[16] = "Quintus";
+$wordsarray[17] = "Roshan";
+$wordsarray[18] = "Silence";
+$wordsarray[19] = "Terabyte";
+$wordsarray[20] = "Underdog";
+$wordsarray[21] = "Volvo";
+$wordsarray[22] = "Winger";
+$wordsarray[23] = "Xiandroid";
+$wordsarray[24] = "Yankees";
+$wordsarray[25] = "Zebrahead";
+//End Word Array Here
+//Begin test code here
+
+$passwordlength = $_POST["number"];
+//for loop to print out password
+$intcount = 0;
+$intposition = 0
+while($intcount < $passwordlength)
+{
+  $intposition = rand(0,25);
+  echo $wordsarray[$intposition];
+  $intcount++;
+}
+?>
+</body>
+
+
